9bb5e95ea630e6f99ee1caeff7fd3b5ad7f01fac
Isabbelle <63454904+Isabbelle@users.noreply.github.com>
2021-03-17 15:30:13 +0100

    Add files via upload

---
+++ coding-bootcamp-testimonials-slider-master/index.js
@@ -0,0 +1,16 @@
+let arr = [45, 9, 90, 58];
+let greatest = 0;
+
+
+for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] > greatest) {
+        greatest = arr[i];
+    }
+ }
+
+console.log(greatest);
+
+
+
+should be the bg img for this div, position relative, 
+       bg repeat no repeat.
